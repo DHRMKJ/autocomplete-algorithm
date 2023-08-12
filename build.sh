b064648994f42197ff0217248e81869c3d59039a
dharmik-bigcircle <dharmik@bigcircle.tech>
2023-08-12 20:29:19 +0530

    folder structure

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-cc -Wall -Werror -std=c11 -pedantic -ggdb -o main main.c
-./main > output/main.dot
+cc -Wall -Werror -std=c11 -pedantic -ggdb -o output/main main.c
+./output/main > output/main.dot
 dot -Tsvg output/main.dot > output/main.svg
 
